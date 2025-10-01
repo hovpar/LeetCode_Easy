ea4e6c63122eec40d785ff9b583210f18fd21054
Hovhannes Partizpanyan <hovpar@icloud.com>
2025-10-01 12:58:41 +0200

    solve preorder and postorder problems in binary trees

--- src/_144BinTreePreOrderTraversal/Solution.java
+++ src/_144BinTreePreOrderTraversal/Solution.java
@@ -2,26 +2,46 @@ package _144BinTreePreOrderTraversal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 class Solution {
 
+//    List<Integer> preorderTraversal(TreeNode root) {
+//        var list = new ArrayList<Integer>();
+//        preorderTraversal(root, list);
+//        return list;
+//
+//    }
+//
+//    private void preorderTraversal(TreeNode node, ArrayList<Integer> list) {
+//        if (node == null) {
+//            return;
+//        } else {
+//            list.add(node.val);
+//            preorderTraversal(node.left, list);
+//            preorderTraversal(node.right, list);
+//
+//        }
+//
+//    }
+
+    // Iterative solution
     List<Integer> preorderTraversal(TreeNode root) {
-        var list = new ArrayList<Integer>();
-        preorderTraversal(root, list);
-        return list;
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        var current = root;
 
-    }
-
-    private void preorderTraversal(TreeNode node, ArrayList<Integer> list) {
-        if (node == null) {
-            return;
-        } else {
-            list.add(node.val);
-            preorderTraversal(node.left, list);
-            preorderTraversal(node.right, list);
-
+        while (!stack.isEmpty() || current != null) {
+            if (current != null) {
+                result.add(current.val);
+                stack.push(current);
+                current = current.left;
+            } else {
+                current = stack.pop();
+                current = current.right;
+            }
         }
-
+        return result;
     }
 
 }
